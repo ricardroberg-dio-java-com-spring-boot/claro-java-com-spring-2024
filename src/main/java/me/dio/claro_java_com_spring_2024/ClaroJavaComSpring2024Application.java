10937f51dc48d654ca3c68ae0c1f2c4a84c43bff
Ricard Roberg <ricardroberg@gmail.com>
2024-11-17 09:21:28 -0300

    Initial commit

---
+++ src/main/java/me/dio/claro_java_com_spring_2024/ClaroJavaComSpring2024Application.java
@@ -0,0 +1,13 @@
+package me.dio.claro_java_com_spring_2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaroJavaComSpring2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClaroJavaComSpring2024Application.class, args);
+	}
+
+}
---
+++ src/test/java/me/dio/claro_java_com_spring_2024/ClaroJavaComSpring2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.claro_java_com_spring_2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClaroJavaComSpring2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
